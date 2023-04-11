b46b4ac3622e5b54f5b92e26e4e5cf804b760868
jcaroper10 <jcaroper@everis.com>
2023-04-11 11:12:06 -0500

    adding scripts

--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-az login --service-principal --username $(USERNAME) --password $(PASSWORD) --tenant $(TENANT)
-
+az login --service-principal --username 5f5f9336-dfce-46cc-8acd-81e6eb9c8a8e --password bie8Q~vS2h9JsgbZbmFKoTOsqcXInf_xND2BIaTj --tenant f524efae-c921-4e8b-851e-b002089d974d
 az devops configure --defaults organization=https://dev.azure.com/ymadrigal2/
 
 az devops configure --defaults project=test
